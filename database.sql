c6ae2f88a735913c1e1c5797a13e249f327dbc6b
PB-Liu <mindware2000@gmail.com>
2023-01-19 21:18:35 -0500

    update sql

--- database.sql
+++ database.sql
@@ -1,54 +1,65 @@
-CREATE TABLE "landlords" (
-  "_id" SERIAL NOT NULL,
-  "name" VARCHAR(255) NOT NULL,
-  "location" VARCHAR(255) NOT NULL,
-  CONSTRAINT "landlords_pk" PRIMARY KEY ("_id")
+CREATE TABLE "public.landlords" (
+	"id" numeric NOT NULL,
+	"name" varchar(255) NOT NULL,
+	"location" varchar(255) NOT NULL,
+	"rating" double(255) NOT NULL,
+	"neighborhood" varchar(255) NOT NULL,
+	CONSTRAINT "landlords_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
-​
-CREATE TABLE "properties" (
-  "_id" SERIAL NOT NULL,
-  "landlord_id" BIGINT NOT NULL,
-  "address" VARCHAR(255) NOT NULL,
-  "city" VARCHAR(255) NOT NULL,
-  "zip" INTEGER,
-  CONSTRAINT "properties_pk" PRIMARY KEY ("_id")
-) WITH (
-  OIDS=FALSE
-);
-​
-CREATE TABLE "reviews" (
-  "_id" SERIAL NOT NULL,
-  "landlord_id" BIGINT NOT NULL,
-  "text" VARCHAR(255),
-  "rating" INTEGER NOT NULL,
-  "would_rent_again" INTEGER NOT NULL,
-  "date" VARCHAR(255) NOT NULL,
-  "user_id" BIGINT NOT NULL,
-  CONSTRAINT "reviews_pk" PRIMARY KEY ("_id")
-) WITH (
-  OIDS=FALSE
-);
-​
-CREATE TABLE "users" (
-  "_id" SERIAL NOT NULL,
-  "username" VARCHAR(255) NOT NULL,
-  "password" VARCHAR(255) NOT NULL,
-  "email" VARCHAR(255) NOT NULL,
-  CONSTRAINT "users_pk" PRIMARY KEY ("_id")
-) WITH (
-  OIDS=FALSE
-);
-​
-ALTER TABLE "reviews" ADD CONSTRAINT "reviews_user_id_foreign" FOREIGN KEY ("user_id") REFERENCES "users" ("_id");
-​
-ALTER TABLE "reviews" ADD CONSTRAINT "reviews_landlord_id_foreign" FOREIGN KEY ("landlord_id") REFERENCES "landlords" ("_id");
-​
-ALTER TABLE "properties" ADD CONSTRAINT "properties_landlord_id_foreign" FOREIGN KEY ("landlord_id") REFERENCES "landlords" ("_id");
 
-INSERT INTO landlords VALUES (1, 'Dummy Landlord1','somewhere1')
-INSERT INTO landlords VALUES (2, 'Dummy Landlord2','somewhere2')
-INSERT INTO landlords VALUES (3, 'Dummy Landlord3','somewhere3')
-INSERT INTO landlords VALUES (4, 'Dummy Landlord4','somewhere4')
-INSERT INTO landlords VALUES (5, 'Dummy Landlord5','somewhere5')
+
+
+CREATE TABLE "public.users" (
+	"username" varchar(255) NOT NULL,
+	"id" numeric(255) NOT NULL,
+	"email" varchar(255) NOT NULL,
+	"password" varchar(255) NOT NULL,
+	CONSTRAINT "users_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public.reviews" (
+	"reviewer_id" varchar(255) NOT NULL,
+	"id" serial(255) NOT NULL,
+	"review" varchar(1000) NOT NULL,
+	"pictures" bytea NOT NULL,
+	"landlord_id" bigint NOT NULL,
+	"rating" numeric NOT NULL,
+	"rent_again" BOOLEAN NOT NULL,
+	"date" DATE NOT NULL,
+	CONSTRAINT "reviews_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public.properties" (
+	"landlord_id" numeric NOT NULL,
+	"id" serial NOT NULL,
+	"address" varchar(255) NOT NULL,
+	"neighborhood" varchar(255) NOT NULL,
+	"zip" numeric(255) NOT NULL,
+	CONSTRAINT "properties_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+
+ALTER TABLE "reviews" ADD CONSTRAINT "reviews_fk0" FOREIGN KEY ("reviewer_id") REFERENCES "users"("id");
+ALTER TABLE "reviews" ADD CONSTRAINT "reviews_fk1" FOREIGN KEY ("landlord_id") REFERENCES "landlords"("id");
+
+ALTER TABLE "properties" ADD CONSTRAINT "properties_fk0" FOREIGN KEY ("landlord_id") REFERENCES "landlords"("id");
+
+
+
+
+
