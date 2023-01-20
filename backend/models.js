882f09cb51634eba6edd0db6c5cc156cd1a445dc
Samantha <samanthaleighmills@gmail.com>
2023-01-19 20:47:24 -0500

    added new db url and password

--- backend/models.js
+++ backend/models.js
@@ -1,16 +1,16 @@
-const { Pool } = require("pg");
+const { Pool } = require('pg')
 
 const PG_URI =
-  "postgres://qjpvwcxx:W_wLRzIkQe8mye7raWmxLNhPu4hw8wqD@salt.db.elephantsql.com/qjpvwcxx";
+  'postgres://bcbrrcch:7njJP3qEeFu7YQYyTKfupNQ6F7ZNMiWp@ruby.db.elephantsql.com/bcbrrcch'
 
 // create a new pool here using the connection string above
 const pool = new Pool({
   connectionString: PG_URI,
-});
+})
 
 module.exports = {
   query: (text, params, callback) => {
-    console.log("executed query", text);
-    return pool.query(text, params, callback);
+    console.log('executed query', text)
+    return pool.query(text, params, callback)
   },
-};
+}
