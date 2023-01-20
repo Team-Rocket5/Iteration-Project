29cf3a2ab3cd175d91c8a41020e22f3b9fbb7311
PB-Liu <mindware2000@gmail.com>
2023-01-20 17:58:38 -0500

    revise routes & controllers; update SQL

--- database.sql
+++ database.sql
@@ -2,7 +2,6 @@ CREATE TABLE public.landlords (
 	"id" serial NOT NULL,
 	"name" varchar(255) NOT NULL,
 	"location" varchar(255) NOT NULL,
-	"rating" numeric(255) NOT NULL,
 	"neighborhood" varchar(255) NOT NULL,
 	CONSTRAINT "landlords_pk" PRIMARY KEY ("id")
 ) WITH (
@@ -27,9 +26,9 @@ CREATE TABLE public.reviews (
 	"reviewer_id" serial NOT NULL,
 	"id" serial NOT NULL,
 	"review" varchar(1000) NOT NULL,
-	"pictures" bytea NOT NULL,
-	"landlord_id" bigint NOT NULL,
-	"rating" numeric NOT NULL,
+	"pictures" bytea,
+	"landlord_id" serial NOT NULL,
+	"rating" serial NOT NULL,
 	"rent_again" BOOLEAN NOT NULL,
 	"date" DATE NOT NULL,
 	CONSTRAINT "reviews_pk" PRIMARY KEY ("id")
@@ -44,7 +43,7 @@ CREATE TABLE public.properties (
 	"id" serial NOT NULL,
 	"address" varchar(255) NOT NULL,
 	"neighborhood" varchar(255) NOT NULL,
-	"zip" numeric(255) NOT NULL,
+	"zip" numeric(255),
 	CONSTRAINT "properties_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
