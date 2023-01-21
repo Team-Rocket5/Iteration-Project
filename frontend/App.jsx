52b31cebc3a5d446bd45e049bc0cc62d477ef15f
ChTiSh <chentina.shen@gmail.com>
2023-01-21 15:14:55 -0500

    change review details component with MUI

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AddReview from './src/components/AddReview.jsx';
+import ReviewDetails from './src/components/ReviewDetails.jsx';
 import LandingPage from './src/pages/LandingPage.jsx';
 
 const App = () => {
@@ -7,7 +9,9 @@ const App = () => {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<LandingPage />} />
+          {/* <Route path='/' element={<LandingPage />} /> */}
+          {/* <Route path='/' element={<AddReview />} /> */}
+          <Route path='/' element={<ReviewDetails />} /> 
         </Routes>
       </BrowserRouter>
     </>
