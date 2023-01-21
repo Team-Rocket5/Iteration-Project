373e8e99c8f75702a5e2719b966e267cd9d99f80
ChTiSh <94409079+ChTiSh@users.noreply.github.com>
2023-01-21 02:09:28 -0500

    Updated AddReview Component UI with MUI (#7)
    * redesign addreview component with MUI
    
    * changed app.jsx back to avoid merge conflicts
    
    Co-authored-by: Felix Leclerc Jr <felixLJR@icloud.com>

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './src/pages/LandingPage.jsx';
 
-
 const App = () => {
   return (
     <>
