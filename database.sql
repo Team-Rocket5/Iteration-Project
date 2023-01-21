24d30758cd626d7a252bc52103d92a4d6124daee
PB-Liu <70303566+PB-Liu@users.noreply.github.com>
2023-01-21 09:09:46 -0500

    Pengbo/edit routes (#5)
    * search route done
    
    * landlord route
    
    * user route
    
    * revise routes & controllers; update SQL
    
    * continue on routes & controllers; update SQL

--- database.sql
+++ database.sql
@@ -2,7 +2,6 @@ CREATE TABLE public.landlords (
 	"id" serial NOT NULL,
 	"name" varchar(255) NOT NULL,
 	"location" varchar(255) NOT NULL,
-	"rating" numeric(255) NOT NULL,
 	"neighborhood" varchar(255) NOT NULL,
 	CONSTRAINT "landlords_pk" PRIMARY KEY ("id")
 ) WITH (
@@ -27,11 +26,12 @@ CREATE TABLE public.reviews (
 	"reviewer_id" serial NOT NULL,
 	"id" serial NOT NULL,
 	"review" varchar(1000) NOT NULL,
-	"pictures" bytea NOT NULL,
-	"landlord_id" bigint NOT NULL,
-	"rating" numeric NOT NULL,
+	"pictures" bytea,
+	"landlord_id" serial NOT NULL,
+	"rating" serial NOT NULL,
 	"rent_again" BOOLEAN NOT NULL,
 	"date" DATE NOT NULL,
+	"address" varchar(255)
 	CONSTRAINT "reviews_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
@@ -44,7 +44,7 @@ CREATE TABLE public.properties (
 	"id" serial NOT NULL,
 	"address" varchar(255) NOT NULL,
 	"neighborhood" varchar(255) NOT NULL,
-	"zip" numeric(255) NOT NULL,
+	"zip" numeric(255),
 	CONSTRAINT "properties_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
