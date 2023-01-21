5649951ad6a3bd06e602c001e336581977c264f4
ChTiSh <chentina.shen@gmail.com>
2023-01-21 01:25:37 -0500

    changed app.jsx back to avoid merge conflicts

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './src/pages/LandingPage.jsx';
-import AddReview from './src/components/AddReview.jsx';
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<AddReview />} />
+          <Route path='/' element={<LandingPage />} />
         </Routes>
       </BrowserRouter>
     </>
