6938d93602f4e4aa6e4bb683a4f021d9d7db6f3b
ChTiSh <chentina.shen@gmail.com>
2023-01-21 01:24:06 -0500

    redesign addreview component with MUI

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './src/pages/LandingPage.jsx';
-
+import AddReview from './src/components/AddReview.jsx';
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<LandingPage />} />
+          <Route path='/' element={<AddReview />} />
         </Routes>
       </BrowserRouter>
     </>
