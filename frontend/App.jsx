563895598618ac147ba77b1aa05cfe7f8066a993
Samantha <samanthaleighmills@gmail.com>
2023-01-21 21:16:49 -0500

    fixed a weird path issue

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -4,13 +4,13 @@ import AddReview from './src/components/AddReview.jsx'
 import ReviewDetails from './src/components/ReviewDetails.jsx'
 import SearchFilter from './src/components/SearchFilter.jsx'
 import LandingPage from './src/pages/LandingPage.jsx'
-import Login from './src/components/Login'
+import Login from '/Users/samanthamills/Desktop/Codesmith/IterationProject/Iteration-Project/frontend/src/components/Login.jsx'
 
 const App = () => {
   return (
     <>
       <Routes>
-        {/* <Route path="/" element={<LandingPage />} /> */}
+        <Route path="/" element={<LandingPage />} />
         {/* <Route path='/' element={<AddReview />} /> */}
         {/* <Route path='/' element={<ReviewDetails />} />  */}
         <Route path="/" element={<SearchFilter />} />
