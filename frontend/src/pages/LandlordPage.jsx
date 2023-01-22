b61872f90e113f0533f181c4c9932d24f1e9af7d
Felix Leclerc Jr <felixLJR@icloud.com>
2023-01-21 20:19:47 -0500

    map added

--- frontend/src/pages/LandlordPage.jsx
+++ frontend/src/pages/LandlordPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import AddReview from '../components/AddReview.jsx'
 import ReviewDetails from '../components/ReviewDetails.jsx';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 
 
 const LandlordPage = () => {
-
+const position = [51.505, -0.09];
 
   return (
     <>
@@ -26,8 +27,21 @@ const LandlordPage = () => {
           </div>
         </div>
         {/* Map and other info section */}
-        <div></div>
-      </div>
+        <div className='leaflet-container'>
+
+            <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+              <TileLayer
+                attribution='&copy; <a href="https://web.archive.org/web/20211121204128/http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+              />
+              <Marker position={position}>
+                <Popup>
+                  A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+              </Marker>
+            </MapContainer>
+          </div>
+        </div>
     </>
   );
 };
